3913e1debc5e6adca1d1b1d958bfcc4737bbd274
Kohmei358 <ajis.kkadoya@gmail.com>
2020-07-09 02:29:19 +0900

    ethan told me nabvar doesn't work in mobile
    I wanted to sleep... lol

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,44 +1,31 @@
 import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
 
 function NavBar(props){
   return(
-    <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "#0F2046"}}>
-      <a className="navbar-brand" href="#" onClick={() => props.gotoPage(0)}>
-        <img src="https://ases.stanford.edu/images/logo.png" width="45" height="30" className="d-inline-block align-top"
-          style={{paddingRight:"15px"}} alt="" />
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home" onClick={() => props.gotoPage(0)}>
+        <img
+        src="https://ases.stanford.edu/images/logo.png"
+        width="45"
+        height="30"
+        className="d-inline-block align-top"
+        alt="Stanford Logo"
+        style={{paddingRight:"15px"}}
+        />
         Stanford ASES
-      </a>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#" onClick={() => props.gotoPage(0)}>Home</Nav.Link>
+          <Nav.Link href="#" onClick={() => props.gotoPage(1)}>Upload</Nav.Link>
+          <Nav.Link href="#" onClick={() => props.gotoPage(2)}>Create Company</Nav.Link>
+          <Nav.Link href="#" onClick={() => props.gotoPage(3)}>Apply</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
 
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <a className="nav-link" onClick={() => props.gotoPage(0)}>
-            Home
-          </a>
-        </li>
-          <li className="nav-item">
-            <a className="nav-link" onClick={() => props.gotoPage(1)}>
-              Upload
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" onClick={() => props.gotoPage(2)}>
-              Create Company
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" onClick={() => props.gotoPage(3)}>
-              Apply
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
   );
 }
 
