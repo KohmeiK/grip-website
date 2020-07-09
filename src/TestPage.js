ed1e4b1964c04ccb7cdd7647ce65711f08f946ba
yhuang4 <yhuang4@andrew.cmu.edu>
2020-07-09 23:38:49 +0800

    test-ethan

--- src/TestPage.js
+++ src/TestPage.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React , {useState, useEffect}from 'react'
 
 function TestPage(){
+  // let auth = firebase.auth();
+  // let db = firebase.firestore(); 
+
+  const  [user, setUser] = useState(true);
+  
+
+  let text =""
+  if(user){
+    text = "Logged In"
+  }else{
+    text = "Logged out"
+  }
+
+  useEffect(function(){
+    setUser(false)
+    
+  })
+
   return(
     <div>
       <h1>
-        Hi
+        enter your email 
       </h1>
+      <h2>
+          {text}
+      </h2>
+      <input type="email" id="email"></input>
+
     </div>
   )
 }
+
+
 
 export default TestPage
