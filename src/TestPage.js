bd374973a62400b8f0d7540270aca716ac6325f9
yhuang4 <yhuang4@andrew.cmu.edu>
2020-07-12 22:46:44 +0800

    basic login screen

--- src/TestPage.js
+++ src/TestPage.js
@@ -1,22 +1,51 @@
-import React , {useState, useEffect, useContext} from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import FirebaseContext from './Firebase'
-// import 'firebase/firestore';
+import 'firebase/firestore';
 
 // import fire from './Fire';
 
- import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, useFormik, Form, Field, ErrorMessage } from 'formik';
 
-function TestPage(){
+function TestPage() {
 
-    useEffect(() => {
 
-    },[]); //Run only onMount
+  // useEffect(() => {
 
-    return(
-        <div>
-          Intentionally Left Blank for Testing
-        </div>
-    )
+  // },[]); //Run only onMount
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email: </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          id="pwd"
+          name="pwd"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.pwd}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
 
 
