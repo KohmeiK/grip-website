6e797115654f251bf0bf883ff7c6d135b5cc6fe3
yhuang4 <yhuang4@andrew.cmu.edu>
2020-07-22 00:12:12 +0800

    Revert "trying firebase admin"
    This reverts commit af368a0dcf8ad15a1be010aeff88b0bd50195bc1.

--- src/Admin.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./testing2-54b27-firebase-adminsdk-wwgva-2e931a21e6.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://testing2-54b27.firebaseio.com"
-});
-
