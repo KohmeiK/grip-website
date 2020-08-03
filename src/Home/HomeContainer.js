a44206de2bb76f2678cc04973934158eedd19c7c
Kohmei358 <ajis.kkadoya@gmail.com>
2020-08-03 18:22:10 +0900

    home screen CSS

--- src/Home/HomeContainer.js
+++ src/Home/HomeContainer.js
@@ -1,14 +1,38 @@
 import React from 'react'
 
+import logo from '../Media/Logo.svg'
 import styles from './HomeContainer.module.css'; // Import css modules stylesheet as styles
+import background from '../Media/HeroIcon-mobile.svg'
 
 function HomeContainer(){
   return(
-    <div className={styles.tagline}>
-      <h1>Bussiness Beyond the Bay</h1>
-      <h5>A subtitle for this main title, more selling points</h5>
-      <button className={styles.mainButton} type="button">Apply</button>
+    <>
+    <div className={styles.wrapper}>
+      <div className={styles.diagonalBox}>
+        <div class="content">
+
+          <ul className={styles.nav}>
+              <li className={styles.logo}>
+                  <img src={logo} alt="Logo"/>
+                  <p><b>Abroad</b></p>
+              </li>
+              <li>Sign in</li>
+          </ul>
+
+          <div className={styles.tagline}>
+              <h1>STANFORD'S GLOBALLY FOCUSED STUDENT ENTRIPRENEURSHIP</h1>
+              <p>Lorem ipsum dolor sit amet. cons adipiscing elit.</p>
+              <div className={styles.buttonWrap}><button className={styles.mainButton} type="button">
+                  APPLY
+              </button></div>
+          </div>
+        </div>
+      </div>
     </div>
+    <div className={styles.diagonalBox}>
+    </div>
+
+    </>
   );
 }
 
