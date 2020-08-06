625291dec2ace57ade2a6141ea1cd5ac948faab5
yhuang4 <yhuang4@andrew.cmu.edu>
2020-08-06 19:14:43 +0800

    still working on forgotPassword

--- src/Signup/ForgotPassword.js
+++ src/Signup/ForgotPassword.js
@@ -2,43 +2,56 @@ import React, { useState, useContext } from 'react'
 import FirebaseContext from '../Firebase'
 import { useHistory, useLocation } from "react-router-dom";
 import { Formik, Field, Form, ErrorMessage } from 'formik';
-import {Button, Spinner} from "react-bootstrap"
+import { Button, Spinner } from "react-bootstrap"
 
 function ForgotPassword() {
   let history = useHistory()
   let location = useLocation()
   const firebase = useContext(FirebaseContext)
   const [isLoading, setLoading] = useState(false)
+  const [message, setMessage] = useState()
 
   let { from } = location.state || { from: { pathname: "/" } };
 
   const handleSubmit = async (values) => {
     try {
-      //wait until log in is complete
-      await firebase.auth.signInWithEmailAndPassword(values.email, values.pwd)
-      setLoading(false)
-      history.replace(from);
+      // //wait until log in is complete
+      // await firebase.auth.signInWithEmailAndPassword(values.email, values.pwd)
+      // setLoading(false)
+      // history.replace(from);
     } catch (error) {
       alert(error)
     }
   }
 
-  
+
   return (
     <div className="w-50 m-auto">
-      <h3>Enter your password</h3>
+      <h3>Enter your email address</h3>
       <Formik
         initialValues={{ email: '' }}
-        onSubmit={async (values, { setSubmitting }) => {
+        onSubmit={async (values, { setSubmitting, resetForm }) => {
+          setTimeout(() => {
+            firebase.auth.sendPasswordResetEmail(values.email).then(function () {
+            setSubmitting(false)
+            resetForm()
+            setMessage('Password reset email sent to the address you provided')
+          }).catch(function (error) {
+            
+            resetForm()
+            alert(error)
+            setSubmitting(false)
+          })
+        }, 1000)
           
         }}
       >
         {({ isSubmitting }) => (
           <Form>
-            <label htmlFor="email">Email Address</label>
             <Field name="email" type="email" />
             <ErrorMessage name="email" />
-            <Button variant="warning" disabled={isSubmitting} type="submit">
+            <br/>
+            <Button disabled={isSubmitting} type="submit">
               {isSubmitting && <Spinner
                 as="span"
                 animation="border"
@@ -46,8 +59,9 @@ function ForgotPassword() {
                 role="status"
                 aria-hidden="true"
               />}
-              Make Developer
+              Submit
             </Button>
+            <h5>{message}</h5>
           </Form>
         )}
       </Formik>
