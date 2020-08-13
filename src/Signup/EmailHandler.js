12f7e22ab706c11add5b5aa8f6cf8c1898a9a040
yhuang4 <yhuang4@andrew.cmu.edu>
2020-08-13 22:29:53 +0800

    direct to login if user ain't logged in
    cleared comments too

--- src/Signup/EmailHandler.js
+++ src/Signup/EmailHandler.js
@@ -9,7 +9,6 @@ import FirebaseContext from '../Firebase/'
 import AuthContext from '../Firebase/AuthContext'
 
 function EmailHandler() {
-    // TODO: Implement getParameterByName()
     function getParameterByName(name, url) {
         if (!url) url = window.location.href;
         name = name.replace(/[\[\]]/g, '\\$&');
@@ -23,13 +22,6 @@ function EmailHandler() {
     const [localDisplay, setLocalDisplay] = useState(null)
     const history = useHistory()
 
-    // Configure the Firebase SDK.
-    // This is the minimum configuration required for the API to be used.
-    // var config = {
-    //     'apiKey': 'AIzaSyBI-HC_K_CzKsv7oTV1ERDNDN3PbkRt6cM' // Copy this key from the web initialization
-    //     // snippet found in the Firebase console.
-    // }
-    // var app = firebase.initializeApp(config);
     var firebase = useContext(FirebaseContext)
     const authContext = useContext(AuthContext)
     var auth = firebase.auth;
@@ -52,16 +44,13 @@ function EmailHandler() {
                 // Display reset password handler and UI.
                 handleResetPassword(auth, actionCode, continueUrl, lang);
                 break;
-            case 'recoverEmail':
-                // Display email recovery handler and UI.
-                handleRecoverEmail(auth, actionCode, lang);
-                break;
             case 'verifyEmail':
                 // Display email verification handler and UI.
                 handleVerifyEmail(auth, actionCode, continueUrl, lang);
                 break;
             default:
-            // Error: invalid mode.
+                // Error: invalid mode.
+                setLocalDisplay(<h3>404 not found</h3>)
         }
     }, [])
 
@@ -75,9 +64,6 @@ function EmailHandler() {
         auth.verifyPasswordResetCode(actionCode).then(function (email) {
             var accountEmail = email;
 
-            // TODO: Show the reset screen with the user's email and ask the user for
-            // the new password.
-            let newPassword = 'abcdefg' // FIX THIS LATER
             setLocalDisplay(
                 <div>
                     <h3>Please enter the new password for {accountEmail}</h3>
@@ -89,19 +75,12 @@ function EmailHandler() {
                             auth.confirmPasswordReset(actionCode, newPassword).then(function (resp) {
                                 // Password reset has been confirmed and new password updated.
 
-                                // TODO: Display a link back to the app, or sign-in the user directly
-                                // if the page belongs to the same domain as the app:
-                                // auth.signInWithEmailAndPassword(accountEmail, newPassword);
                                 setLocalDisplay(
                                     <div>
                                         <h3>Password Reset. You can now log in with the new password.</h3>
                                         <Button onClick={() => history.push('/login')}>Go to login</Button>
                                     </div>
                                 )
-
-                                // TODO: If a continue URL is available, display a button which on
-                                // click redirects the user back to the app via continueUrl with
-                                // additional state determined from that URL's parameters.
                             }).catch(function (error) {
                                 // Error occurred during confirmation. The code might have expired or the
                                 // password is too weak.
@@ -158,96 +137,38 @@ function EmailHandler() {
         });
     }
 
-    function handleRecoverEmail(auth, actionCode, lang) {
-        // Localize the UI to the selected language as determined by the lang
-        // parameter.
-        var restoredEmail = null;
-        // Confirm the action code is valid.
-        auth.checkActionCode(actionCode).then(function (info) {
-            // Get the restored email address.
-            restoredEmail = info['data']['email'];
 
-            // Revert to the old email.
-            return auth.applyActionCode(actionCode);
-        }).then(function () {
-            // Account email reverted to restoredEmail
-
-            // TODO: Display a confirmation message to the user.
-
-            // You might also want to give the user the option to reset their password
-            // in case the account was compromised:
-            auth.sendPasswordResetEmail(restoredEmail).then(function () {
-                // Password reset confirmation sent. Ask user to check their email.
-            }).catch(function (error) {
-                // Error encountered while sending password reset code.
-            });
-        }).catch(function (error) {
-            // Invalid code.
-        });
-    }
-
-    async function handleVerifyEmail(auth, actionCode, continueUrl, lang) {
+    function handleVerifyEmail(auth, actionCode, continueUrl, lang) {
         // Localize the UI to the selected language as determined by the lang
         // parameter.
         // Try to apply the email verification code.
 
-        // try{
-        //     await auth.applyActionCode(actionCode)
-        //     setLocalDisplay(
-        //         <div>
-        //             <h3>Email verified. Click on the button below to continue</h3>
-        //             <Button onClick={() => history.push('/firstUpload')}>Continue</Button>
-        //         </div>
-        //     )
-        // } catch (error){
-        //     setLocalDisplay(
-        //         <h3>{error.message}</h3>
-        //     )
-        // }
-
         auth.applyActionCode(actionCode).then(function (resp) {
             // Email address has been verified.
 
-            // TODO: Display a confirmation message to the user.
-            // You could also provide the user with a link back to the app.
 
-            // history.push('/firstUpload')
-
-            // let user = await auth.currentUser;
-            // await user.reload();
-            // user = await auth.currentUser;
-            // while(!user.isEmailVerified){
-            //     console.log('not verified')
-            // }
-            // history.push('/firstUpload')
-
-            //Just to avoid more than 1 inverval
-            if (intervalId) {
-                clearInterval(intervalId);
+            if (auth.currentUser) { // user is logged in already
+                //Just to avoid more than 1 inverval
+                if (intervalId) {
+                    clearInterval(intervalId);
+                }
+                intervalId = setInterval(() => {
+                    auth.currentUser
+                        .reload()
+                        .then(ok => {
+                            if (auth.currentUser.emailVerified) {
+                                authContext.forceUserUpdate(auth.currentUser)
+                                clearInterval(intervalId);
+                                history.push("/firstUpload")
+                            }
+                        })
+                }, 1000)
+            } else {
+                history.push("/login")
             }
-            intervalId = setInterval(() => {
-                auth.currentUser
-                    .reload()
-                    .then(ok => {
-                        if (auth.currentUser.emailVerified) {
-                            authContext.forceUserUpdate(auth.currentUser)
-                            clearInterval(intervalId);
-                            history.push("/firstUpload")
-                        }
-                    })
-            }, 1000)
 
-            // setLocalDisplay(
-            //     <div>
-            //         <h3>Email verified. Click on the button below to continue</h3>
-            //         <a href="./firstUpload">Continue</a>
-            //         {/* <Button onClick={() => history.push('/firstUpload')}>Continue</Button> */}
-            //     </div>
-            // )
+            setLocalDisplay(<h3>Redirecting...</h3>)
 
-            // TODO: If a continue URL is available, display a button which on
-            // click redirects the user back to the app via continueUrl with
-            // additional state determined from that URL's parameters.
         }).catch(function (error) {
             // Code is invalid or expired. Ask the user to verify their email address
             // again.
