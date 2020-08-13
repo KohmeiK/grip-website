85f6ac0e63cca36414f62760cc28f5896fae09a7
Kohmei358 <ajis.kkadoya@gmail.com>
2020-08-13 19:59:14 +0900

    trying docs hosting again

---
+++ docs/Applications/Applications.js
@@ -0,0 +1,101 @@
+import React, { useState, useContext, useEffect } from 'react'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
+import FirebaseContext from '../Firebase'
+import AuthContext from '../Firebase/AuthContext'
+
+import JobCardForStudent from './JobCardForStudent'
+
+/**
+ * More info here
+ */
+function Applications() {
+    const firebase = useContext(FirebaseContext)
+    const authContext = useContext(AuthContext)
+    const [jobs, setJobs] = useState([]) //Data from DB
+    const [loading, setLoading] = useState(true); //Still loading array
+
+  /**
+   * Insert text at cursor position.
+   *
+   * @param {string} text
+   * @public
+   */
+    const updateJobs = async (doc) => {
+        let jobsBuildingArray = []
+        let jobIDs = doc.data().jobsAppliedTo
+        await Promise.all(jobIDs.map(async (jobID, index) => {
+            let jobRef = firebase.db.collection('jobs').doc(jobID)
+            await getJob(jobRef, index, jobsBuildingArray)
+        }))
+
+        setJobs(jobsBuildingArray)
+        setLoading(false)
+    }
+
+    const getJob = async (jobRef, index, outputArray) => {
+        let doc = await jobRef.get()
+        // jobs.push(doc.data())
+        // setJobs(jobs.concat(doc.data()))
+        outputArray[index] = doc.data()
+        return("Ethan - You must return something to a promise (when you await soemthing)")
+    }
+
+    useEffect(() => {
+        //Only on mount
+        let studentRef = firebase.db.collection('students').doc(authContext.user.uid)
+        studentRef.get().then(function (doc) {
+            if(doc.data().jobsAppliedTo === null){
+              alert("You do not have a jobsAppliedTo array. Aborting updateJobs")
+            } else if(doc.data().jobsAppliedTo.length === 0){
+              alert("You have not applied to any jobs! Aborting updateJobs")
+            }else{
+              updateJobs(doc)
+            }
+        }).catch(function (error) {
+            console.log(error)
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    let localDisplay = "Loading..."
+    if (!loading) {
+        localDisplay = jobs.map((job, index) => { //Convert each element to JSX
+            //convert all elements before reach render, this is only updates when show is changed
+            return (
+                <JobCardForStudent
+                    key={index}
+                    index={index}
+                    title={job.title}
+                    companyName={job.companyName}
+                    dl={job.deadline}
+                />
+            );
+        })
+    }
+
+    return (
+        <div style={{ background: "#e0e0e0" }}>
+            <h1>A student page where you can see your applications</h1>
+            <Container fluid style={{ paddingTop: "2em" }}>
+                <Row>
+                    <Col>
+                        <div style={{ marginLeft: "1em", borderRadius: "25px", background: "white", height: "40em" }}>
+                            <div style={{ margin: "2em", marginTop: "0em", background: "white", height: "40em" }}>
+                                Search Options Go Here
+            </div>
+                        </div>
+                    </Col>
+                    <Col sm={7}>
+                        {localDisplay}
+                    </Col>
+                    <Col sm={3}>More Settings</Col>
+                </Row>
+            </Container>
+        </div >
+    )
+}
+
+export default Applications
---
+++ docs/styleguide.config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  title: 'Styleguidist ASES Global',
+
+  sections: [
+    {
+      name: 'Preface',
+      sections: [
+        {
+          name: 'Installation / Setup',
+          content: 'docs/installation.md',
+          description: 'This section explains how to set up your developmemt enviroment'
+        },
+        {
+          name: 'UI Design Guide',
+          content: 'docs/styleguide.md',
+          description: 'This section is a UI style guide for when you want to add more pages to the site while keeping it themactically consistent'
+        },
+        {
+          name: 'Misc',
+          content: 'docs/misc.md',
+          description: 'Other useful info that I had no where else to put'
+        },
+        {
+          name: 'Live Site',
+          external: true,
+          href: 'http://kolledgeholdings.com'
+        }
+      ]
+    },
+    {
+      name: 'Admin',
+      components: '../Admin/*.js',
+      description: 'Pages that only admin accounts can acsess'
+      // exampleMode: 'expand', // 'hide' | 'collapse' | 'expand'
+      // usageMode: 'expand' // 'hide' | 'collapse' | 'expand'
+    },
+    {
+      name: 'Applications',
+      components: './Applications/*.js',
+      description: '(Previously Your Applications) - show a list of companies a student has applied to'
+    },
+    {
+      name: 'Apply',
+      components: 'src/Apply/*.js',
+      description: 'Job list page where studens can see a list of jobs they can apply to'
+    },
+    {
+      name: 'Company',
+      components: 'src/Company/*.js',
+      description: 'Pages only company accounts can acsess'
+    },
+    {
+      name: 'Firebase',
+      components: 'src/Firebase/*.js',
+      description: 'Context for firebase services'
+    },
+    {
+      name: 'Home',
+      components: 'src/Home/*.js',
+      description: 'Landing page, which contains (How it works, about, FAQ, testimonials, contact)'
+    },
+    {
+      name: 'Nav',
+      components: 'src/Nav/*.js',
+      description: 'Anything to do with the nav bar'
+    },
+    {
+      name: 'Route',
+      components: 'src/Route/*.js',
+      description: 'React Router Stuff and acess error pages'
+    },
+    {
+      name: 'Signup',
+      components: 'src/Signup/*.js',
+      description: 'Log in / Sign up + New account onboarding'
+    },
+    {
+      name: 'User',
+      components: 'src/User/*.js',
+      description: '(Previously My Profile) Anything under the My Info'
+    },
+  ]
+};
