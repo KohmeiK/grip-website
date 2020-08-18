995e13ed9a8bc4eb2a75ed5451fd5df8cd8c71b9
yhuang4 <yhuang4@andrew.cmu.edu>
2020-08-19 00:16:07 +0800

    restored companyCreation

--- src/Admin/CompanyCreationContainer.js
+++ src/Admin/CompanyCreationContainer.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useContext } from "react"
+import React, { useEffect, useContext, useState } from "react"
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Col, Row, Container, Spinner } from 'react-bootstrap'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 import FirebaseContext from '../Firebase'
 
@@ -9,6 +11,18 @@ import FirebaseContext from '../Firebase'
  */
 function CompanyCreationContainer() {
   const firebase = useContext(FirebaseContext)
+  const [logoURL, setLogoURL] = useState('')
+  const [textValue, setTextValue] = useState('')
+  const handleChange = value => {
+    setTextValue(value);
+  };
+
+  /**
+ * Insert text at cursor position.
+ *
+ * @param {string} text
+ * @public
+ */
 
   useEffect(() => {
     //This is called every time the component shows up on the screen
@@ -16,46 +30,56 @@ function CompanyCreationContainer() {
   }, []);
 
   const handleSubmit = async (values, setSubmitting, resetForm) => {
-      try{
-        console.log("RequestSent")
-        console.log(values,"values")
-        const createNewCompany = firebase.functions.httpsCallable('createNewCompany')
-        const result = await createNewCompany({formVals: values})
-        console.log(result.data.message, "result.data.message")
-        setSubmitting(false)
-        resetForm()
-        alert("Keep of a copy of the Company ID!         " + result.data.message)
-        return(null);
+    try {
+      console.log("RequestSent")
+      console.log(values, "values")
+      const createNewCompany = firebase.functions.httpsCallable('createNewCompany')
+      const result = await createNewCompany({ formVals: values })
+      console.log(result.data.message, "result.data.message")
+      setSubmitting(false)
+      resetForm()
+      alert("Keep of a copy of the Company ID!         " + result.data.message)
+      return (null);
 
-      }catch(error){
-        console.log(error, "Error")
-        setSubmitting(false)
-        alert(error.message)
-        return("error");
-      }
+    } catch (error) {
+      console.log(error, "Error")
+      setSubmitting(false)
+      alert(error.message)
+      return ("error");
+    }
   }
 
   return (
-    <div style={{ background: "#ba916e", marginTop:"85px"}} >
+    <div style={{ background: "#ba916e", marginTop: "85px" }} >
       <Container fluid style={{ paddingTop: "2em" }}>
         <Row>
           <Col>
             <div style={{ marginLeft: "1em", borderRadius: "25px", background: "white", height: "40em" }}>
               <div style={{ margin: "2em", marginTop: "0em", background: "white", height: "40em" }}>
-                Margin Intentionally Left Blank
+                <h5>Comapny Creation Reminder: </h5>
+                1. Comapny Info uses markdown language. <br />
+                   - You can access different formattings at the toolbar, including
+                bold/header/list. <br />
+                   - For all lines except those for headers and lists, type two spaces at the end to make a line break.<br />
+                   - Upon completion, click the eye icon in the tool bar to preview how it will be displayed. (Make sure you do this before submitting!)<br />
+                   - If you accidentally expand the typearea, simiply press esc key. <br />
+                <br />
+                2. To retrieve company logo url, upload the image on the right; the textbox will be filled automatically. <br />
+                <br />
+                3. It takes some time after you click submit; you will be alerted when the process is completed.
             </div>
             </div>
           </Col>
           <Col sm={5}>
             <div>
               <Formik
-                initialValues={{ name: '', info: '', email: '', pwd: '',}}
+                initialValues={{ name: '', info: '', email: '', pwd: '', url: '' }}
                 validate={values => {
                   const errors = {};
                   if (!values.name) {
                     errors.name = 'Required';
                   }
-                  if (!values.info) {
+                  if (!textValue) {
                     errors.info = 'Required';
                   }
                   if (!values.email) {
@@ -64,11 +88,18 @@ function CompanyCreationContainer() {
                   if (!values.pwd) {
                     errors.pwd = 'Required';
                   }
+                  if (!logoURL) {
+                    errors.url = 'Required; upload the company logo on the right';
+                  }
                   return errors;
                 }}
                 onSubmit={(values, { setSubmitting, resetForm }) => {
-                  // resetForm()
-                  // alert(JSON.stringify(values, null, 2))
+                  values.info = textValue
+                  values.url = logoURL
+                  alert(JSON.stringify(values, null, 2))
+                  resetForm()
+                  setTextValue('')
+                  setLogoURL('') // a part of resetForm
                   handleSubmit(values, setSubmitting, resetForm)
                 }}
 
@@ -80,7 +111,8 @@ function CompanyCreationContainer() {
                     <ErrorMessage name="name" component="div" />
                     <br />
                     Company Info: <br />
-                    <Field type="text" name="info" as="textarea" style={{ width: "100%" }} />
+                    <SimpleMDE value={textValue} onChange={handleChange} />
+                    {/* <Field type="text" name="info" value={textValue} style={{ width: "100%" }} /> */}
                     <ErrorMessage name="info" component="div" />
                     <br />
                     Preferred Login Email: <br />
@@ -91,81 +123,9 @@ function CompanyCreationContainer() {
                     <Field type="text" name="pwd" style={{ width: "100%" }} />
                     <ErrorMessage name="pwd" component="div" />
                     <br />
-
-                    <button className="my-2 btn btn-primary bg-wb" type="submit" disabled={isSubmitting}>
-                    {isSubmitting && <Spinner
-                      as="span"
-                      animation="border"
-                      size="sm"
-                      role="status"
-                      aria-hidden="true"
-                      />}
-                      Submit
-                    </button>
-                  </Form>
-                )}
-              </Formik>
-            </div>
-
-            {/* add a job to a company */}
-            <div>
-              <Formik
-                initialValues={{ companyID: '', jobTitle: '', jobInfo: '', jobDl: '' }}
-                validate={values => {
-                  const errors = {};
-                  if (!values.companyID) {
-                    errors.companyID = 'Required';
-                  }
-                  if (!values.jobTitle) {
-                    errors.jobTitle = 'Required';
-                  }
-                  if (!values.jobInfo) {
-                    errors.jobInfo = 'Required';
-                  }
-                  if (!values.jobDl) {
-                    errors.jobDl = 'Required';
-                  }
-                  return errors;
-                }}
-                onSubmit={async(values, { setSubmitting, resetForm }) => {
-                  try{
-                    console.log("RequestSent")
-                    console.log(values,"values")
-                    const addNewJob = firebase.functions.httpsCallable('addNewJob')
-                    const result = await addNewJob({formVals: values})
-                    console.log(result.data.message, "result.data.message")
-                    resetForm()
-                    setSubmitting(false)
-                    alert(result.data.message)
-                    return(null);
-
-                  }catch(error){
-                    console.log(error, "Error")
-                    setSubmitting(false)
-                    alert(error.message)
-                    return("error");
-                  }
-                }}
-              >
-                {({ isSubmitting }) => (
-                  <Form>
-                    <h4>Add a Job to a Company</h4>
-                    Company ID: <br />
-                    <Field type="text" name="companyID" style={{ width: "100%" }} />
-                    <ErrorMessage name="companyID" component="div" />
-                    <br />
-                    Job Title: <br />
-                    <Field type="text" name="jobTitle" style={{ width: "100%" }} />
-                    <ErrorMessage name="jobTitle" component="div" />
-                    <br />
-                    Job Info <br />
-                    <Field type="text" name="jobInfo" as="textarea" style={{ width: "100%" }} />
-                    <ErrorMessage name="jobInfo" component="div" />
-                    <br />
-                    Job Deadline: <br />
-                    <Field type="text" name="jobDl" style={{ width: "100%" }} />
-                    <ErrorMessage name="jobDl" component="div" />
-
+                    Company Logo URL: <br />
+                    <Field type="text" name="url" style={{ width: "100%" }} value={logoURL} disabled />
+                    <ErrorMessage name="url" component="div" />
 
                     <button className="my-2 btn btn-primary bg-wb" type="submit" disabled={isSubmitting}>
                       {isSubmitting && <Spinner
@@ -174,7 +134,7 @@ function CompanyCreationContainer() {
                         size="sm"
                         role="status"
                         aria-hidden="true"
-                        />}
+                      />}
                       Submit
                     </button>
                   </Form>
@@ -183,7 +143,50 @@ function CompanyCreationContainer() {
             </div>
 
           </Col>
-          <Col sm={4}>More Settings</Col>
+          <Col sm={4}> {/* place for uploading company logo */}
+            <Formik
+              initialValues={{ file: null }}
+
+              validate={values => {
+                const errors = {};
+                if (!values.file) {
+                  errors.file = 'Required';
+                }
+                return errors;
+              }}
+
+              onSubmit={(values, { resetForm, setSubmitting }) => {
+                let logo = values.file
+                let storageRef = firebase.storage
+                let uid = new Date().getTime()
+                console.log(uid + '/' + values.file.name)
+                let logoRef = storageRef.child(uid + '/' + values.file.name) // name the file with its original name, under a folder with unique name
+                logoRef.put(logo).then(async function () {
+                  logoRef.getDownloadURL().then(function (url) {
+                    setLogoURL(url)
+                    setSubmitting(false)
+                    resetForm()
+                  })
+                })
+              }}>
+              {({ values, isSubmitting, handleSubmit, setFieldValue }) => (
+                <form onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="file">Company Logo Upload</label>
+                    <input id="file" name="file" type="file" accept="image/*" onChange={(event) => {
+                      setFieldValue("file", event.currentTarget.files[0]);
+                    }} className="form-control" />
+                    <ErrorMessage name="file" component="div" />
+                  </div>
+                  <br />
+                  {isSubmitting
+                    ? <button type="submit" className="btn btn-dark" disabled>Uploading</button>
+                    : <button type="submit" className="btn btn-primary">Submit</button>}
+                </form>
+
+              )}
+            </Formik>
+          </Col>
         </Row>
       </Container>
     </div>
